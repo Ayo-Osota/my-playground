37bd04c507fce5321cd5184cd11e6f6af50dee99
Ayo Osota <osotaayomikun@gmail.com>
2025-01-18 19:22:10 +0100

    hashmap

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,5 +1,88 @@
-// enum is like ts type
+/// This is a documentation comment, it will be used to generate documentation for the code.
 
+// Option is like ts union type Some(<T>) | None
+
+// String is owned and used in struct, &str is borrowed and used in function
+
+// derive is a special macro used on enums and structs 
+// to automatically implement traits like 
+// Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, and Default.
+
+#[derive(Debug)]
+enum MenuChoice {
+    MAinMenu,
+    Start,
+    Quit
+}
+
+fn get_choice(input: &str) -> Result<MenuChoice, String> {
+    match input {
+        "mainmenu" => Ok(MenuChoice::MAinMenu),
+        "start" => Ok(MenuChoice::Start),
+        "quit" => Ok(MenuChoice::Quit),
+        _ => Err("Invalid choice".to_owned())
+    }
+}
+
+enum Discount {
+    Percent(i32),
+    Flat(i32)
+}
+
+struct Ticket {
+    event: String,
+    price: i32,
+}
+
+
+// vector is like array but can grow in size
+fn vector () {
+    let mut my_numbers: Vec<i32> = Vec::new();
+    my_numbers.push(5);
+    my_numbers.push(6);
+    my_numbers.pop();
+    println!("number of elements is {}", my_numbers.len());
+    println!("{:?}", my_numbers);
+
+    let my_boys = vec!["James", "Cody", "Gakpo"];
+
+    for boy in &my_boys {
+        println!("{}", boy)  
+    }
+
+    // using SLF
+    match &my_boys.is_empty() {
+        true => println!("There are girls"),
+        false => println!("There are boys")
+    }
+}
+
+// impl is like ts class
+impl Person {
+    fn new(name: String, age: i32) -> Person {
+        Person {
+            name,
+            age: age as u8,
+            direction: Direction::Up
+        }
+    }
+    fn say_hello(&self) {
+        println!("Hello, my name is {}!", self.name);
+    }  
+}
+
+// tuple is like array but can also be destructured like objects
+fn tuple () {
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+  
+    let (x, y, _z) = tup;  // destructuring
+    println!("The value of y is: {}", y);
+    println!("The value of x is: {}", x);
+    println!("The value of z is: {}", tup.2); // access by index
+}
+
+// enum is like ts type
+#[derive(Clone)]
 enum Direction {
     Up,
     Down,
@@ -10,7 +93,8 @@ enum Direction {
 // struct is like ts interface
 struct Person {
     name: String,
-    age: u8
+    age: u8,
+    direction: Direction
 }
 
 fn repetition_while () {
@@ -64,7 +148,47 @@ fn conditional () {
 fn main() {
     println!("Hello, world!");
 
+    let n = 3;
+    match n {
+        3 => println!("three"),
+        other => print!("not three {}", other),
+    };
+
+    let flat = Discount::Flat(2);
+
+    match flat {
+        Discount::Flat(2) => println!("Flat discount of 2!"),
+        Discount::Flat(amount) => println!("Flat discount of {}!", amount),
+        _ => (),
+    }
+
+    let concert = Ticket {
+        event: "Concert".to_owned(),
+        price: 50,
+    };
+
+    match concert {
+        Ticket { price: 50, event } => println!("50 event is {}", event),
+        Ticket { price, .. } => println!("The price is ${}", price),
+    }
+
+    let gakpo = Person::new(String::from("James"), 25);
+    gakpo.say_hello();
+
     let player_direction = Direction::Down;
+
+    let player = Person {
+        name: String::from("Cody"),
+        age: 25,
+        direction: player_direction.clone()
+    };
+
+    println!("{} is {} years old and is facing {}", player.name, player.age,  match player.direction {
+        Direction::Up => String::from("up!"),
+        Direction::Down => String::from("down!"),
+        Direction::Left => String::from("left!"),
+        Direction::Right => String::from("right!"),
+    });
 
     match player_direction {
         Direction::Up => println!("Moving up!"),
@@ -73,6 +197,11 @@ fn main() {
         Direction::Right => println!("Moving right!"),
     }
 
+   let choice = get_choice("mainmenu err");
+
+   println!("choice = {:?}", choice);
+    vector();
+    tuple();
     repetition_while();
     repetition();
     match_keyword();
